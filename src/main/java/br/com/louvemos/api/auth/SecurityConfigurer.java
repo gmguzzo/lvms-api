c609b12b18c08a9f31d846ed5094df5ad8e3a5bb
gabriel mourao <gabrielmourao95@gmail.com>
2021-03-28 20:56:11 -0300

    Adjusts

--- src/main/java/br/com/louvemos/api/auth/SecurityConfigurer.java
+++ src/main/java/br/com/louvemos/api/auth/SecurityConfigurer.java
@@ -35,7 +35,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     private final String ENV = System.getenv("ENV");
 
     private final String[] permittedPaths = {"/v2/auth", "/", "/v2/persons/self"};
-    private final String[] adminOnlyPOSTPUTDELETEPaths = {"/v2/**/**"};
+    private final String[] adminOnlyPOSTPUTDELETEPaths = {"/v2/**", "/v2/**/**"};
     private final String[] userPermittedPOSTPUTDELETEPaths = {"/v2/songs", "/v2/albums", "/v2/setlists", "/v2/songsetlists", "/v2/persons/share", "/v2/persons/unshare"};
 
     @Override
