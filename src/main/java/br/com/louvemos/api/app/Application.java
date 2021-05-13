0b0eb91778bb829f57e2e3e720ba321113d93f5b
gabriel mourao <gabrielmourao95@gmail.com>
2021-05-13 20:41:42 -0300

    Port postgres to mysql

--- src/main/java/br/com/louvemos/api/app/Application.java
+++ src/main/java/br/com/louvemos/api/app/Application.java
@@ -4,10 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "br.com.louvemos")
 @EntityScan("br.com.louvemos.*")
+@PropertySources({
+    @PropertySource(value = "classpath:/application-${ENV}.properties", ignoreResourceNotFound = false)
+})
 public class Application {
 
     public static void main(String[] args) {
